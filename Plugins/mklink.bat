18e81257f48e9913c75ff36cbb704dc8ccd6d89e
endink <sharping@outlook.com>
2023-02-10 13:22:35 +0800

    add makelink.bat

---
+++ Plugins/mklink.bat
@@ -0,0 +1,10 @@
+set DIR="F:\3D_Works\UE\MediaPipe4U\Build"
+
+mklink /D "%~dp0MediaPipe4U" "%DIR%\MediaPipe4U"
+mklink /D "%~dp0MediaPipe4UGStreamer" "%DIR%\MediaPipe4UGStreamer"
+mklink /D "%~dp0GStreamer" "%DIR%\GStreamer"
+mklink /D "%~dp0MediaPipe4ULiveLink" "%DIR%\MediaPipe4ULiveLink"
+mklink /D "%~dp0MediaPipe4UBVH" "%DIR%\MediaPipe4UBVH"
+mklink /D "%~dp0MediaPipe4UNvAR" "%DIR%\MediaPipe4UNvAR"
+
+pause > nul
